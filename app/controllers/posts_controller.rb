8c830c8b4c665cf2df63e2f6da0b6bf988a8b884
Manofsteel <abhiy.yadav7@gmail.com>
2018-03-10 05:04:11 +0530

    rails action,controller and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
-	def :index
+	def index
+		@content_first = "This is first content";
+		@content_second = "This is content second";
+	end
+	def new
+		
+	end
+	def create
+		
+	end
+	def edit
+		
+	end
+	def update
+		
+	end
+	def show
+		
+	end
+	def destroy
 		
 	end
 end
