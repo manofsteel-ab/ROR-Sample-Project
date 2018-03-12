d5da82c0119d19a3f9f85df39cba6be813aeeca4
Manofsteel <abhiy.yadav7@gmail.com>
2018-03-13 01:51:19 +0530

    database association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+	def Category
+		params.require(:Category).permit(:name)
+	end
+	# attr_accessible :name
+	has_many :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+	def Post
+		params.require(:Category).permit(:title, :body, :category_id, :author_id)
+	end	
+	# attr_accessible :name , :body, :category_id, :author_id
+	belongs_to :category
 end
