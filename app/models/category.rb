ce9292692a7e47ada5c491d60eb300db2fe90c80
Manofsteel <abhiy.yadav7@gmail.com>
2018-03-14 01:42:55 +0530

    removing extra files

--- app/models/category.rb
+++
@@ -1,7 +0,0 @@
-class Category < ApplicationRecord
-	def Category
-		params.require(:Category).permit(:name)
-	end
-	# attr_accessible :name
-	has_many :post
-end
--- app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-	def Post
-		params.require(:Category).permit(:title, :body, :category_id, :author_id)
-	end	
-	# attr_accessible :name , :body, :category_id, :author_id
-	belongs_to :category
-end
