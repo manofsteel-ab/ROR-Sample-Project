d4a0ba00d16e3b47bfcd00e3fdb11666a3c19710
Manofsteel <abhiy.yadav7@gmail.com>
2018-03-14 00:28:54 +0530

    database create without using terminal

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,25 @@
 class PostsController < ApplicationController
+	def post_params
+		params.require(:post).permit! 
+		# this will permit anything
+	end
 	def index
 		@posts = Post.all
 	end
 	def show
-		@posts = Post.find(params[:id]);
+		@posts = Post.find(params[:id])
 	end
 	def new
-		
+		@post = Post.new
+		@category = Category.all;
 	end
 	def create
-		
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path, :notice => "Your Post has been saved" 
+		else
+			render "new"
+		end
 	end
 	def edit
 		
